df0299ea0f8ca5b50322f00cfa996bffa9f14b28
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-11-03 19:26:18 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
