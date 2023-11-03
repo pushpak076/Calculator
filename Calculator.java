756f63803cb62ef4a587370ea904110fccc7fb0e
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-11-03 19:25:46 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,79 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Enter first number: ");
+                double num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number: ");
+                double num2 = scanner.nextDouble();
+
+                System.out.print("Enter operation (+, -, *, /, sqrt, pow, fact): ");
+                String operation = scanner.next();
+
+                double result = 0;
+
+                switch (operation) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 == 0) {
+                            throw new ArithmeticException("Division by zero is not allowed.");
+                        }
+                        result = num1 / num2;
+                        break;
+                    case "sqrt":
+                        result = Math.sqrt(num1);
+                        break;
+                    case "pow":
+                        result = Math.pow(num1, num2);
+                        break;
+                    case "fact":
+                        result = factorial((int) num1);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid operation");
+                }
+
+                System.out.println("Result: " + result);
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter valid numbers.");
+                scanner.next(); // Clear the invalid input
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print("Do you want to perform another calculation? (yes/no): ");
+            String choice = scanner.next().toLowerCase();
+
+            if (!choice.equals("yes")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static double factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+}
